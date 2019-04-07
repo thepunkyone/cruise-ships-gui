2fcc177718e4b8cddb7f97ea5eaeae69ffe60934
Vitnija Bluzma <thepunkyone@gmail.com>
2019-04-07 19:39:02 +0100

    changed ports module to ES6 syntax

--- src/port.js
+++ src/port.js
@@ -1,22 +1,25 @@
 (function exportPort() {
 
-  function Port(name) {
-    this.name = name;
-    this.ships = [];
-  }
-  
-  Port.prototype.addShip = function (ship) {
-    this.ships.push(ship);
-  };
-  
-  Port.prototype.removeShip = function (ship) {
-    if (this.ships.includes(ship)) {
-      const i = this.ships.indexOf(ship);
-      this.ships.splice(i, 1);
-    } else {
-      throw new Error(`${ship.name} not found at the port!`);
+  class Port {
+
+    constructor(name) {
+      this.name = name;
+      this.ships = [];
     }
-  };
+
+    addShip(ship) {
+      this.ships.push(ship);
+    }
+
+    removeShip(ship) {
+      if (this.ships.includes(ship)) {
+        const i = this.ships.indexOf(ship);
+        this.ships.splice(i, 1);
+      } else {
+        throw new Error(`${ship.name} not found at the port!`);
+      }
+    }
+  }
 
   if (typeof module !== 'undefined' && module.exports) {
     module.exports = Port;
